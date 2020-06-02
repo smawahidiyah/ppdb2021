ecb46907dc8ed541e248bf5b9aca7fa45fe509ed
hajirasyafi <hajirasyafi@gmail.com>
2020-06-02 16:33:44 +0700

    no message

--- resources/views/panel/login.blade.php
+++ resources/views/panel/login.blade.php
@@ -26,7 +26,7 @@
       <form action="{{route('postlogin')}}" method="post">
         @csrf
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="email">
+          <input type="text" class="form-control" placeholder="Username" name="email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
